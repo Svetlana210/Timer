67e1a6f03611a202358591a4cae171ef81933d79
Svetlana210 <svetik.may24@gmail.com>
2022-12-17 16:56:41 +0100

    add1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let timerId = null;
+
+const onStartBtn = () => {
+  refs.startBtn.setAttribute('disabled', true);
+  refs.stopBtn.removeAttribute('disabled');
+  timerId = setInterval(() => {
+    getRandomHexColor();
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+};
+
+const onStopBtn = () => {
+  refs.startBtn.removeAttribute('disabled');
+  refs.stopBtn.setAttribute('disabled', true);
+  clearInterval(timerId);
+};
+
+refs.startBtn.addEventListener('click', onStartBtn);
+refs.stopBtn.addEventListener('click', onStopBtn);
